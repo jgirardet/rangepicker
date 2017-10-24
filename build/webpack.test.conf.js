0666d01864b3ddfa9474c8ce2eddce1f069c8d83
Jimmy Girardet <ijkl@tuta.io>
2017-10-24 16:03:08 +0200

    tout

--- build/webpack.test.conf.js
+++ build/webpack.test.conf.js
@@ -3,15 +3,16 @@
 //
 // const utils = require('./utils')
 // const webpack = require('webpack')
-// const merge = require('webpack-merge')
-// const baseWebpackConfig = require('./webpack.base.conf')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
 //
-// const webpackConfig = merge(baseWebpackConfig, {
+const webpackConfig = merge(baseWebpackConfig, {
 //   // use inline sourcemap for karma-sourcemap-loader
-//   module: {
+  // module: {
 //     rules: utils.styleLoaders()
 //   },
-//   devtool: '#inline-source-map',
+  devtool: 'inline-cheap-module-source-map',
+  externals: [require('webpack-node-externals')()]
 //   resolveLoader: {
 //     alias: {
 //       // necessary to to make lang="scss" work in test when using vue-loader's ?inject option
@@ -24,15 +25,15 @@
 //       'process.env': require('../config/test.env')
 //     })
 //   ]
-// })
+})
 
 // test specific setups
-if (process.env.NODE_ENV === 'test') {
-  module.exports.externals = [require('webpack-node-externals')()]
-  module.exports.devtool = 'inline-cheap-module-source-map'
-}
+// if (process.env.NODE_ENV === 'test') {
+//   module.exports.externals = [require('webpack-node-externals')()]
+//   module.exports.devtool = 'inline-cheap-module-source-map'
+// }
 //
 // // no need for app entry during tests
 // delete webpackConfig.entry
 //
-// module.exports = webpackConfig
+module.exports = webpackConfig
